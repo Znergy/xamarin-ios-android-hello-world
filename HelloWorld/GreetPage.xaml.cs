62b27ef4df12debacfd738705045b019fb0b51ad
znergy <ryanjonesirl@gmail.com>
2017-06-18 00:07:20 -0700

    add button that ques a pop up box

---
+++ HelloWorld/GreetPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace HelloWorld
+{
+    public partial class GreetPage : ContentPage
+    {
+        public GreetPage()
+        {
+            InitializeComponent();
+        }
+
+		void Handle_Clicked(object sender, System.EventArgs e)
+		{
+			DisplayAlert("Title", "Hello World", "OK");
+		}
+    }
+}
